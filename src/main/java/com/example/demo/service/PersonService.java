7fbff6c6dccef4c53db8dcb077e210dcbd62a464
yuriykolomiyets <juriy121@gmail.com>
2022-08-03 22:43:13 +0200

    24:39

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.IPersonDao;
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final IPersonDao iPersonDao;
+
+    public PersonService(IPersonDao iPersonDao) {
+        this.iPersonDao = iPersonDao;
+    }
+
+    public int addPerson(Person person){
+    return iPersonDao.insertPerson(person);
+    }
+}
